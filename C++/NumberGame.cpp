1c9bc407cdb936a7b21b8dd5a080f015be833442
anfelder <andrewfelder95@gmail.com>
2015-11-24 12:45:26 -0600

    added start of c++ guess the number

---
+++ C++/NumberGame.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cstdlib>
+#include <string>
+
+using namespace std;
+
+int getMax(){
+	cout << "Enter the upper bound: ";
+	int max;
+	cin >> max;
+}
+int main(){
+
+	return 0;
+}
